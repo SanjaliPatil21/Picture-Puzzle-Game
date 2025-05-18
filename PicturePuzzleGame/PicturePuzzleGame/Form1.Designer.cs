fe947b71b936be7d7ae243699e25d472278b39a7
SanjaliPatil21 <SP138924@GMAIL.COM>
2025-05-18 13:43:55 +0530

    Add files via upload

---
+++ PicturePuzzleGame/PicturePuzzleGame/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace PicturePuzzleGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PicturePuzzleGame.Properties.Resources._8;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(131, 127);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "8";
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::PicturePuzzleGame.Properties.Resources._6;
+            this.pictureBox2.Location = new System.Drawing.Point(149, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(131, 127);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "6";
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::PicturePuzzleGame.Properties.Resources._4;
+            this.pictureBox3.Location = new System.Drawing.Point(286, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(131, 127);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "4";
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::PicturePuzzleGame.Properties.Resources._2;
+            this.pictureBox4.Location = new System.Drawing.Point(12, 145);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(131, 127);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "2";
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::PicturePuzzleGame.Properties.Resources._7;
+            this.pictureBox5.Location = new System.Drawing.Point(149, 145);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(131, 127);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Tag = "7";
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::PicturePuzzleGame.Properties.Resources._5;
+            this.pictureBox6.Location = new System.Drawing.Point(286, 145);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(131, 127);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Tag = "5";
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::PicturePuzzleGame.Properties.Resources._3;
+            this.pictureBox7.Location = new System.Drawing.Point(12, 278);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(131, 127);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Tag = "3";
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::PicturePuzzleGame.Properties.Resources._1;
+            this.pictureBox8.Location = new System.Drawing.Point(149, 278);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(131, 127);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Tag = "1";
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = global::PicturePuzzleGame.Properties.Resources._0;
+            this.pictureBox9.Location = new System.Drawing.Point(286, 278);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(131, 127);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Tag = "0";
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = global::PicturePuzzleGame.Properties.Resources.Main;
+            this.pictureBox10.Location = new System.Drawing.Point(485, 53);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(184, 178);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 9;
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.Tag = "Main";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.8F);
+            this.lblTimer.Location = new System.Drawing.Point(478, 278);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(246, 39);
+            this.lblTimer.TabIndex = 10;
+            this.lblTimer.Text = "Time Left : 60s";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(446, 344);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 11;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click_1);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(560, 344);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 12;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click_1);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(669, 344);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(75, 23);
+            this.btnRestart.TabIndex = 13;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
---
+++ PicturePuzzleGame/PicturePuzzleGame/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PicturePuzzleGame
+{
+    public partial class Form1 : Form
+    {
+        int timeLeft = 60;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            if((string)pictureBox6.Tag=="0")
+            {
+                Swap(pictureBox9, pictureBox6);
+            }
+            else if((string)pictureBox8.Tag=="0")
+            {
+                Swap(pictureBox9 , pictureBox8);
+            }
+            Check();
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            if((string)pictureBox9.Tag == "0")
+            {
+                Swap(pictureBox8, pictureBox9);
+            }
+            else if((string)pictureBox5.Tag == "0")
+            {
+                Swap(pictureBox8 , pictureBox5);
+            }
+            else if((string)pictureBox7.Tag == "0")
+            {
+                Swap(pictureBox8 , pictureBox7);
+            }
+            Check();
+        }
+
+        private void Swap(PictureBox picA, PictureBox picB)
+        {
+            PictureBox picT = new PictureBox();
+            picT.Image = picA.Image;
+            picT.Tag = picA.Tag;
+            picA.Image = picB.Image;
+            picA.Tag = picB.Tag;
+            picB.Image = picT.Image;
+            picB.Tag = picT.Tag;
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            if((string)pictureBox8.Tag=="0")
+            {
+                Swap(pictureBox7, pictureBox8);
+            }
+            else if((string)pictureBox4.Tag=="0")
+            {
+                Swap(pictureBox7 , pictureBox4);
+                 
+            }
+            Check();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if((string)pictureBox1.Tag=="0")
+            {
+                Swap(pictureBox4, pictureBox1);
+            }
+            else if((string)pictureBox5.Tag=="0")
+            {
+                Swap(pictureBox4, pictureBox5);
+            }
+            else if((string)pictureBox7.Tag=="0")
+            {
+                Swap(pictureBox4 , pictureBox7);
+            }
+            Check();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if((string)pictureBox2.Tag=="0")
+            {
+                Swap(pictureBox1, pictureBox2);
+            }
+            else if((string)pictureBox4.Tag=="0")
+            {
+                Swap(pictureBox1, pictureBox4);
+            }
+            Check();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if((string)pictureBox1.Tag=="0")
+            {
+                Swap(pictureBox2, pictureBox1);
+            }
+            else if((string)pictureBox5.Tag=="0")
+            {
+                Swap(pictureBox2, pictureBox5);
+            }
+            else if((string)pictureBox3.Tag=="0")
+            {
+                Swap(pictureBox2, pictureBox3);
+            }
+            Check();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if ((string)pictureBox2.Tag == "0")
+            {
+                Swap(pictureBox3, pictureBox2);
+            }
+            else if ((string)pictureBox6.Tag == "0")
+            {
+                Swap(pictureBox3, pictureBox6);
+            }
+            Check();
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            if((string)pictureBox3.Tag=="0")
+            {
+                Swap(pictureBox6 , pictureBox3);
+            }
+            else if((string)pictureBox5.Tag=="0")
+            {
+                Swap(pictureBox6, pictureBox5);
+            }
+            else if ((string)pictureBox9.Tag =="0")
+            {
+                Swap(pictureBox6, pictureBox9);
+            }
+            Check();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            if((string)pictureBox2.Tag=="0")
+            {
+                Swap(pictureBox5 , pictureBox2);
+            }
+            else if ((string)pictureBox4.Tag == "0")
+            {
+                Swap(pictureBox5, pictureBox4);
+            }
+            else if ((string)pictureBox6.Tag == "0")
+            {
+                Swap(pictureBox5, pictureBox6);
+            }
+            else if ((string)pictureBox8.Tag == "0")
+            {
+                Swap(pictureBox5, pictureBox8);
+            }
+            Check();
+        }
+
+        public void Check()
+        {
+            if((string)pictureBox1.Tag=="1" && (string)pictureBox2.Tag=="2" && 
+                (string)pictureBox3.Tag == "3" && (string)pictureBox4.Tag == "4" && 
+                (string)pictureBox5.Tag == "5" && (string)pictureBox6.Tag == "6" && 
+                (string)pictureBox7.Tag == "7" && (string)pictureBox8.Tag == "8" && 
+                (string)pictureBox9.Tag =="0")
+            {
+                timer1.Stop();
+                MessageBox.Show("You have completed the puzzle on time!!!", "Congratulations", MessageBoxButtons.OK);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            timeLeft--;
+            lblTimer.Text = "Time Left: " + timeLeft + "s";
+
+            if (timeLeft <= 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Time's up! Try again.", "Game Over", MessageBoxButtons.OK);
+               
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timeLeft = 60;
+            lblTimer.Text = "Time Left: 60s";
+            timer1.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            MessageBox.Show("Timer stopped!", "Info", MessageBoxButtons.OK);
+        }
+
+        private void ResetPuzzle()
+        {
+            // Prepare list of image-tag pairs
+            List<(Image img, string tag)> tiles = new List<(Image, string)>()
+    {
+        (Properties.Resources._1, "1"),
+        (Properties.Resources._2, "2"),
+        (Properties.Resources._3, "3"),
+        (Properties.Resources._4, "4"),
+        (Properties.Resources._5, "5"),
+        (Properties.Resources._6, "6"),
+        (Properties.Resources._7, "7"),
+        (Properties.Resources._8, "8"),
+        (Properties.Resources._0, "0")
+    };
+
+            // Shuffle the list
+            Random rnd = new Random();
+            tiles = tiles.OrderBy(x => rnd.Next()).ToList();
+
+            // Assign shuffled images to PictureBoxes
+            PictureBox[] boxes = {
+        pictureBox1, pictureBox2, pictureBox3,
+        pictureBox4, pictureBox5, pictureBox6,
+        pictureBox7, pictureBox8, pictureBox9
+    };
+
+            for (int i = 0; i < boxes.Length; i++)
+            {
+                boxes[i].Image = tiles[i].img;
+                boxes[i].Tag = tiles[i].tag;
+            }
+        }
+
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            ResetPuzzle();            
+            timeLeft = 60;            
+            lblTimer.Text = "Time Left: 60s";
+            timer1.Start();
+        }
+
+        private void btnStop_Click_1(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            MessageBox.Show("Timer stopped!", "Info", MessageBoxButtons.OK);
+        }
+
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+            timeLeft = 60;
+            lblTimer.Text = "Time Left: 60s";
+            timer1.Start();
+        }
+    }
+}
